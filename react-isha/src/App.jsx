f2a1550c4e07671ca33c98538af9e4e89035ad82
IshaT-Hub <ishatiwari0508@gmail.com>
2024-12-16 23:19:28 +0530

    new

--- react-isha/src/App.jsx
+++ react-isha/src/App.jsx
@@ -12,22 +12,28 @@ export const App = () => {
 };
 
 const NetflixCards = () => {
+  const name = "The Lord of Rings";
+  const rating = "8.2";
+  const summary =
+    "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet";
+
+  const returnGenre = () => {
+    const genre = "RomCom";
+    return genre;
+  };
+
   return (
     <div>
       <div>
-        <img
-          src="../public/lotrimg.jpeg"
-          alt="lort imggg"
-          height="100%"
-          width="40%"
-        />
+        <img src="/lotrimg.jpeg" alt="lort imggg" height="100%" width="40%" />
       </div>
-      <h1>Lord of Rings</h1>
-      <h2>Rating 8.2</h2>
-      <h3>
-        Summary Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor sit amet
-      </h3>
+      <h1>Name: {name}</h1>
+      {/* use of dynamic variables in react */}
+      <h2>Rating: {5+8}</h2>
+      {/* use of dynamic expressions in react */}
+      <h3>Summary: {summary}</h3>
+      <p>Genre: {returnGenre()}</p>
+      {/* use of dynamic function calls in react */}
     </div>
   );
 };
