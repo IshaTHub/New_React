b68f4b19d2c102658f0c776973448b39aa32880e
IshaTHub <ishatiwari0508@gmail.com>
2025-03-29 01:17:06 +0530

    props, childrens, export defaults

--- react-isha/src/App.jsx
+++ react-isha/src/App.jsx
@@ -1,49 +1,17 @@
-import React from "react";
-
-export const App = () => {
-  return (
-    <React.Fragment>
-      <NetflixCards />
-      <NetflixCards />
-      <NetflixCards />
-      <NetflixCards />
-    </React.Fragment>
-  );
-};
-
-const NetflixCards = () => {
-  const name = "The Lord of Rings";
-  const rating = "8.2";
-  const summary =
-    "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet";
-
-  const returnGenre = () => {
-    const genre = "RomCom";
-    return genre;
-  };
-
-  const AgeLimit = () => {
-    if (age >= 25) return "can definately watch this movie";
-    return "cannot watch this movie";
-  };
-
-  const age = 28;
+import Button from "./Button.jsx";
+const App = () => {
+  const handleClick = () =>{
+    console.log("i was clicked from parent");
+  }
   return (
     <div>
-      <div>
-        <img src="/lotrimg.jpeg" alt="lort imggg" height="100%" width="40%" />
-      </div>
-      <h1>Name: {name}</h1>
-      {/* use of dynamic variables in react */}
-      <h2>Rating: {5 + 8}</h2>
-      {/* use of dynamic expressions in react */}
-      <h3>Summary: {summary}</h3>
-      <p>Genre: {returnGenre()}</p>
-      {/* use of dynamic function calls in react */}
-      <button>{age >= 18 ? "Availabe" : "Not Available"}</button>
-      <button>{AgeLimit()}</button>
+      <Button data={{a:{b:{c:'123'}}}} clickAction={handleClick}>Click me</Button>  {/* we are recieving clickAction as a prop from Button */}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
+
+// data={{a:{b:{c:'123'}}}} clickAction={handleClick} are children of Button component.
+// we are passing data and clickAction as props to Button component.
--- react-isha/src/main.jsx
+++ react-isha/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Practice from "./Practice.jsx";
+//import Practice from "./Practice.jsx";
+import App from "./App.jsx";
 // import "./tailwind.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Practice />
+   <App />
   </StrictMode>
 );
