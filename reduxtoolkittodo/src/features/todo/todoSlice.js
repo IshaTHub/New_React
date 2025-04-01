f76ad695cd31e3edbd28fda3b28489f22625b456
IshaT-Hub <ishatiwari0508@gmail.com>
2025-04-01 17:39:11 +0530

    reducers

--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -16,6 +16,16 @@ export const todoSlice = createSlice({
             }
             state.todos.push(todo); //push the todo to the todos array
         },
-        removeTodo: () =>{},
+        removeTodo: (state, action) =>{
+            state.todos = state.todos.filter(todo => todo.id !== action.payload); //filter out the todo with the given id
+        
+        },
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload;
+            const todo = state.todos.find(todo => todo.id === id);
+            if (todo) {
+                todo.text = text;
+            }
+        }
     }
 })
